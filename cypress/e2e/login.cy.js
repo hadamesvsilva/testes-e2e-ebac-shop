316790dd78f85cd40a180f9117a590204a49940c
hadamesvsilva <ulttrrass@gmail.com>
2023-02-22 11:20:21 -0300

    primeiro commit - Funcionalidade Login

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,42 @@
+/// <reference types="cypress" />
+const perfil = require('../fixtures/perfil.json')
+
+beforeEach(() => {
+    cy.visit('minha-conta')
+});
+
+afterEach(() => {
+    cy.screenshot()
+});
+
+context('Funcionalidade Login', () => {
+    it('Deve fazer login com sucesso - Usando arquivo de dados', () => {
+        cy.get('#username').type (perfil.usuario)
+        cy.get('#password').type (perfil.senha)
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.page-title').should('contain' , 'Minha conta')
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, aluno_ebac')
+    });
+
+    it('Login com sucesso usando Comando customizado', () => {
+        cy.login(perfil.usuario, perfil.senha)
+        cy.get('.page-title').should('contain', 'Minha conta')
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, aluno_ebac')
+    })
+
+    it('Login usando fixture', () => {
+        cy.fixture('perfil').then((dados) => {
+            cy.login(dados.usuario, dados.senha)
+            cy.get('.page-title').should('contain', 'Minha conta')
+            cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, aluno_ebac')
+        })
+    })
+
+    it('Deve fazer login com sucesso - sem otimização', () => {
+        cy.get('#username').type(perfil.usuario)
+        cy.get('#password').type(perfil.senha, { log: false })
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.page-title').should('contain', 'Minha conta')
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, aluno_ebac')
+    })
+})
